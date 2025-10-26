456a477214c790947aaf7381e5bbb5c24bab8308
pello198 <kwemoipello@gmail.com>
2025-10-26 14:12:38 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,5 @@
 -- Insert multiple customers
+USE alx_book_store;
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
